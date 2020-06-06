944fe5b8f72434b5988ec82b50c2d55b89dd4c22
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-06 18:09:09 +0200

    feat: SignIn section almost completed.
    The SignIn section has already set up functionality and styles.
    User has authentication logic, redirection and admin control.

--- src/core/Signin.js
+++ src/core/Signin.js
@@ -1,12 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { signin, authenticate, isAuthenticated } from "./apiCore";
+import { Redirect } from "react-router-dom";
+
 import "./Signin.css";
 import Navigation from "../layout/Navigation";
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToReferrer: false,
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true,
+          });
+        });
+      }
+    });
+  };
+
+  const signInForm = () => (
+    <form className="sign-box">
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          value={name}
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          value={email}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          onChange={handleChange("password")}
+          value={password}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Sign Up
+      </button>
+    </form>
+  );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      if (user && user.role === 1) {
+        return <Redirect to="/admin/dashboard" />;
+      } else {
+        return <Redirect to="/" />;
+      }
+    }
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
+  };
+
   return (
     <>
       <Navigation />
-      <h4> SIGN in FROM</h4>
+      <div className="mt-5">
+        <h4 className="text-center mb-5"> Log In</h4>
+        {signInForm()}
+        {redirectUser()}
+      </div>
     </>
   );
 };
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -10,3 +10,39 @@ export const getVideogames = () => {
     })
     .catch((err) => console.log(err));
 };
+
+export const signin = (user) => {
+  return fetch(`${API}/users/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
