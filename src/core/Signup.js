f080c7c792bedd87f1de3aaafd4afee579a323d2
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-07 12:18:10 +0200

    feat: signUp form created. Works properly
    Now the project has a SignUp section. Routes are connected. It works okay

--- src/core/Signup.js
+++ src/core/Signup.js
@@ -1,25 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../layout/Navigation";
+import { Link } from "react-router-dom";
+import { signup } from "./apiCore";
 
 import "./Signup.css";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, success, error } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true,
+        });
+      }
+    });
+  };
+
   const signUpForm = () => (
     <form className="sign-box">
       <div className="form-group">
         <label className="text-muted">Name</label>
-        <input type="text" className="form-control" />
+        <input
+          onChange={handleChange("name")}
+          value={name}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label className="text-muted">email</label>
-        <input type="email" className="form-control" />
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          value={email}
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          onChange={handleChange("password")}
+          value={password}
+          type="password"
+          className="form-control"
+        />
       </div>
-      <button className="btn btn-primary">Sign Up</button>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Sign Up
+      </button>
     </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      {" "}
+      New Account Successfully Created You can now
+      <Link to="/signin">Sign in</Link>
+    </div>
   );
 
   return (
@@ -28,6 +100,8 @@ const Signup = () => {
       <div className="mt-5">
         <h4 className="text-center mb-5">Signup form</h4>
         {signUpForm()}
+        {showError()}
+        {showSuccess()}
       </div>
     </>
   );
